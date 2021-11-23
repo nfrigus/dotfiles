cb9319afa1da99ceeff51aa1e004f96e92e0e0a0
Semen Shestakov <shestakovsemen@gmail.com>
2021-11-24 00:55:53 +0200

    Add timer functions

--- bash/alias.sh
+++ bash/alias.sh
@@ -103,3 +103,20 @@ apt-find() {
     apt search $* | grep -vE '^$' | sed -re 'N;s,/.*\n,|,' | column -ts '|' | grep $*
 }
 
+countdown(){
+    date1=$((`date +%s` + $1));
+    while [ "$date1" -ge `date +%s` ]; do
+    ## Is this more than 24h away?
+    days=$(($(($(( $date1 - $(date +%s))) * 1 ))/86400))
+    echo -ne "$days day(s) and $(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+    sleep 0.1
+    done
+}
+stopwatch(){
+    date1=`date +%s`;
+    while true; do
+    days=$(( $(($(date +%s) - date1)) / 86400 ))
+    echo -ne "$days day(s) and $(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r";
+    sleep 0.1
+    done
+}
