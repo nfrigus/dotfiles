b782cd69774c15deb1ef5fd313ad8bd3ca92d74a
Semen Shestakov <shestakovsemen@gmail.com>
2023-02-09 22:09:19 +0200

    core: Add confirmations and logging for symlinking phase

--- lib/core/cli.sh
+++ lib/core/cli.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 ROOT_DIR="$(realpath -L -- "$(dirname -- "$0")/../..")"
+cache=()
+
+# 1 - ERROR
+# 2 - WARNING
+# 3 - INFO
+# 4 - DEBUG
+LOG_LEVEL=3
 
 
 main() {
+    if [[ "$*" = *" -v"* ]]; then
+        LOG_LEVEL=4
+    fi
+
     case $1 in
         install)
             cmd_install
@@ -16,7 +27,6 @@ main() {
 
 cmd_install() {
     cd "$ROOT_DIR"
-    MODULES="$(find_modules)"
 
     mkdir -p "$ROOT_DIR/var/bash" "$ROOT_dir/usr/bin"
 
@@ -36,13 +46,17 @@ print_help() {
 set_symlinks() {
     local k
     local v
-    for module in ${MODULES[@]}; do
-        get_module_config $module \
-        | awk '/^link /' \
-        | cut -d\  -f2- \
-        | while read -r entry; do
-            k="$(echo $entry | cut -d' ' -f1)"
-            v="$(echo $entry | cut -d' ' -f2)"
+    local entries
+    for module in $(list_modules); do
+        readarray -t entries < <(
+            get_module_config $module \
+            | awk '/^link /' \
+            | cut -d\  -f2-
+        )
+
+        for entry in "${entries[@]}"; do
+            k="$(echo "$entry" | cut -d\  -f1 )"
+            v="$(echo "$entry" | cut -d\  -f2- | xargs)"
             case $v in
                 "\$ROOT")
                     v="$ROOT_DIR"
@@ -77,7 +91,7 @@ get_module_local_path() {
 
 cache_paths() {
     local path
-    for module in ${MODULES[@]}; do
+    for module in $(list_modules); do
         path="$HOME/dotfiles/$(get_module_local_path $module)/bin"
         [ -d "$path" ] && echo "$path"
 
@@ -104,7 +118,7 @@ cache_paths() {
 }
 
 cache_sources() {
-    for module in ${MODULES[@]}; do
+    for module in $(list_modules); do
         get_module_config $module \
         | awk '/^source /' \
         | cut -d\  -f2- \
@@ -127,30 +141,72 @@ cache_sources() {
     done > "$ROOT_DIR/var/bash/sources"
 }
 
-# todo: Promt confirmations
-# todo: Debug logs
 set_symlink() {
     local source="$1"
     local target="$2"
 
+    log debug set_symlink.call "$source => $target"
+
     if [ -L "$source" ]; then
-        if [ "$(readlink "$source")" = "$target" ]; then
-            : # skip already valid link
+        local value="$(readlink "$source")"
+        if [ "$value" = "$target" ]; then
+            log info "Symlink synced" "$source -> $target"
         else
-            # Replacing existing link
-            ln -sf "$target" "$source"
+            confirm "$source is link to $value. Overwrite it to target $target?" \
+            && ln -sf "$target" "$source" \
+            && log info "Symlink updated" "$source -> $target"
         fi
     elif [ -f "$source" ]; then
-        # Replacing existing file
-        ln -sf "$target" "$source"
+        confirm "$source already exists. Confirm overwriting?" \
+        && ln -sf "$target" "$source" \
+        && log info "Symlink created" "$source -> $target"
     else
-        # Creating a link
-        ln -s "$target" "$source"
+        ln -s "$target" "$source" \
+        && log info "Symlink created" "$source -> $target"
     fi
 }
 
-find_modules() {
-    ls -d1 lib/*/ | xargs -n1 basename
+confirm() {
+    local msg=${1:-Proceed?}
+    read -p "> $msg [yn]" -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        return 1
+    fi
+}
+
+list_modules() {
+    if [ -z "${cache[modules]}" ]; then
+        cache[modules]=$(ls -d1 lib/*/ | xargs -n1 basename)
+    fi
+
+    echo "${cache[modules]}"
+}
+
+log() {
+    # Black        0;30     Dark Gray     1;30
+    # Red          0;31     Light Red     1;31
+    # Green        0;32     Light Green   1;32
+    # Brown/Orange 0;33     Yellow        1;33
+    # Blue         0;34     Light Blue    1;34
+    # Purple       0;35     Light Purple  1;35
+    # Cyan         0;36     Light Cyan    1;36
+    # Light Gray   0;37     White         1;37
+    local level="$1"
+    local context="$2"
+    shift 2
+    local message="$*"
+
+    case "$level" in
+        debug) level=4 ;;
+        info)  level=3 ;;
+        warn)  level=2 ;;
+        error) level=1 ;;
+    esac
+
+    [ "$LOG_LEVEL" -ge "$level" ] \
+    && echo -e "\033[0;36m$context\033[0m: $message"
 }
 
 main "$@"
